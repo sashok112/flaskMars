d3ff2d3cd4b8c725f479251d48ade60251d627ed
Alexandr <s.stepantzover@yandex.ru>
2022-03-18 22:20:48 +0300

    id adress

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# flask_ngrok_example.py
-from flask import Flask
-from flask_ngrok import run_with_ngrok
+from flask import Flask, url_for
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+@app.route('/image_mars')
+def image():
+    return f'''
+    <title>Привет, Марс!</title>
+    <h1>Жди нас, Марс!</h1>
+    <img src="{url_for('static', filename='img/mars_planet.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+     <br>Вот она, какая красивая планета!</br>'''
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, host='127.0.0.1')
